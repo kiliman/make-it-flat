03e5e8ec423c6bbaecdc47f337f2614ee57553cb
Michael Carter <kiliman@gmail.com>
2022-11-03 09:01:25 -0400

    fix: ignore all files in routes folder
    By default Remix will treat files in the *routes* folder using the current convention. You need to ignore those files so `remix-flat-routes` can generate the correct routing structure.

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,7 @@
 const { flatRoutes } = require('remix-flat-routes');
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ["**/.*"],
+  ignoredRouteFiles: ["**/*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
